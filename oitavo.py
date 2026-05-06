d19ac0867f2fe1a49e4dc398f9376c767957f356
Vitória Viana <vitorialegal552@gmail.com>
2026-05-06 11:26:01 -0300

    caça erros

--- oitavo.py
+++ oitavo.py
@@ -100,8 +100,33 @@
 #     print("Reprovado, KKKKK")
 
 # 8. O Contador de 1 a 5
-Objetivo: Mostrar na tela os números 1, 2, 3, 4 e 5.
-for i in range(5):
-print(i)
+# Objetivo: Mostrar na tela os números 1, 2, 3, 4 e 5.
+# for i in range(5):
+# print(i)
 
 #CORRIGIDO
+# for i in range (1,5):
+#     print(i)
+
+# 9. O Loop Eterno
+# tentativas = 1
+# while tentativas <= 3:
+# print("Tentando conectar...")
+# O código deveria parar após 3 tentativas
+#Corrigido
+# tentativas= 1
+# while tentativas <= 3:
+#     print("Tentando conectar ...")
+#     tentativas += 1
+
+# 10. A Senha Teimosa
+# O programa deve pedir a senha até que o usuário digite "python123"
+# senha = ""
+# while senha == "python123":
+# senha = input("Digite a senha secreta: ")
+# print("Acesso concedido!") 
+#Corrigido
+# senha = input("Digite a senha secreta")
+# while senha == "python123":
+#     break
+# print("Acesso concedido!")
